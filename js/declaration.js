8f60d1b888d69114957bc1e2a744a865b3e5c7b2
elhajjame <el-hajjame@outlook.com>
2026-02-09 12:27:16 +0100

    declaration javascript is finished, waiting form merge and optimize it

--- js/declaration.js
+++ js/declaration.js
@@ -1,10 +1,19 @@
 const addBtn = document.querySelector('.add-btn');
 const overlay = document.querySelector('.overlay');
 const formModal = document.querySelector('.form-container');
+const form = document.querySelector('.form');
 
+
+//------------------- select html inputs --------------------
 const selectedOption = document.querySelector('#selectOpt')
+const formEl = document.querySelector('.form');
+const delayDaysEl = document.querySelector('#delay-days');
+const penaltyAmountEl = document.querySelector('#penalty-amount');
+const submitBtn = document.querySelector('.submit-btn');
+const declarationDateEl = document.querySelector('#dateEl');
 
-// ------------- Data ----------------
+// ------------- getting data from local storage ----------------
+let saveData = JSON.parse(localStorage.getItem('saveData')) || [];
 
 const employers = [
     { id: 1, companyName: "Tech Solutions Ltd", businessSector: "IT Services" },
@@ -14,7 +23,7 @@ const employers = [
     { id: 5, companyName: "EduSmart Academy", businessSector: "Education" }
 ];
 
-
+//------------ display data on select --------------
 employers.forEach(elm => {
     const option = document.createElement('option');
     option.value = elm.id;
@@ -22,26 +31,37 @@ employers.forEach(elm => {
     selectedOption.appendChild(option);
 });
 
-
-//------------------- select html inputs --------------------
-const formEl = document.querySelector('.form');
-const delayDaysEl = document.querySelector('#delay-days');
-const penaltyAmountEl = document.querySelector('#penalty-amount');
-const submitBtn = document.querySelector('.submit-btn');
-const declarationDateEl = document.querySelector('#dateEl');
-
-console.log(selectedOption.value);
-//----------------------\
-formEl.addEventListener('submit', function () {
+//-------------- check inputs,push data function ---------------
+const checkPushData = function () {
     const delayDays = delayDaysEl.value;
-    const Employers = selectedOption.value
-    const declarationDate = declarationDateEl.value
-    const EmployerText = selectedOption.options[selectedOption.selectedIndex].text
+    const declarationDate = declarationDateEl.value;
+    const EmployerText = selectedOption.options[selectedOption.selectedIndex].text;
+    const allInputs = document.querySelectorAll('.input-box');
 
-    closeForm();
-})
+    let isValid = true;
+    allInputs.forEach(input => {
+        if (input.value.trim() === '') {
+            isValid = false;
+            input.style.borderColor = '#FA5C5C'
+        } else input.style.borderColor = '#ddd'
+    });
 
-//------------ open, close form --------------
+    if (isValid) {
+        console.log("push");
+        saveData.push({
+            companyName: EmployerText,
+            delayDays: delayDays,
+            declarationDate: declarationDate,
+            penaltyAmount: penaltyAmountEl.value
+        });
+        closeForm();
+    }
+    localStorage.setItem('saveData', JSON.stringify(saveData));
+    console.log(saveData);
+    form.reset();
+}
+
+//------------ open, close form functions --------------
 const openForm = function () {
     overlay.classList.remove('hidden');
     formModal.classList.remove('hidden');
@@ -50,8 +70,8 @@ const openForm = function () {
         const delayDays = delayDaysEl.value;
         if (delayDays) {
             if (delayDays > 0) {
-                let penaltyAmount = delayDays * 50
-                penaltyAmountEl.value = `${penaltyAmount} DH`
+                var penaltyAmountResult = delayDays * 50
+                penaltyAmountEl.value = `${penaltyAmountResult} DH`
             }
         }
     })
@@ -60,5 +80,10 @@ const closeForm = function () {
     overlay.classList.add('hidden');
     formModal.classList.add('hidden');
 }
-addBtn.addEventListener('click', openForm)
-overlay.addEventListener('click', closeForm)
+
+//------------- call function in DOM event ---------------------
+formEl.addEventListener('submit', checkPushData);
+addBtn.addEventListener('click', openForm);
+overlay.addEventListener('click', closeForm);
+
+
