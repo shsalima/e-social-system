144d2f0ed401ab48a2115baea7f4780fb31a2a33
elhajjame <el-hajjame@outlook.com>
2026-02-09 10:51:10 +0100

    open and close modal function, claculate penalty amount function and display the amount

--- js/declaration.js
+++ js/declaration.js
@@ -2,14 +2,63 @@ const addBtn = document.querySelector('.add-btn');
 const overlay = document.querySelector('.overlay');
 const formModal = document.querySelector('.form-container');
 
-//
-const openForm = function(){
+const selectedOption = document.querySelector('#selectOpt')
+
+// ------------- Data ----------------
+
+const employers = [
+    { id: 1, companyName: "Tech Solutions Ltd", businessSector: "IT Services" },
+    { id: 2, companyName: "Green Farms Inc", businessSector: "Agriculture" },
+    { id: 3, companyName: "Urban Builders Co", businessSector: "Construction" },
+    { id: 4, companyName: "HealthPlus Clinics", businessSector: "Healthcare" },
+    { id: 5, companyName: "EduSmart Academy", businessSector: "Education" }
+];
+
+
+employers.forEach(elm => {
+    const option = document.createElement('option');
+    option.value = elm.id;
+    option.text = `${elm.companyName} (${elm.businessSector})`
+    selectedOption.appendChild(option);
+});
+
+
+//------------------- select html inputs --------------------
+const formEl = document.querySelector('.form');
+const delayDaysEl = document.querySelector('#delay-days');
+const penaltyAmountEl = document.querySelector('#penalty-amount');
+const submitBtn = document.querySelector('.submit-btn');
+const declarationDateEl = document.querySelector('#dateEl');
+
+console.log(selectedOption.value);
+//----------------------\
+formEl.addEventListener('submit', function () {
+    const delayDays = delayDaysEl.value;
+    const Employers = selectedOption.value
+    const declarationDate = declarationDateEl.value
+    const EmployerText = selectedOption.options[selectedOption.selectedIndex].text
+
+    closeForm();
+})
+
+//------------ open, close form --------------
+const openForm = function () {
     overlay.classList.remove('hidden');
     formModal.classList.remove('hidden');
+
+    delayDaysEl.addEventListener('input', function () {
+        const delayDays = delayDaysEl.value;
+        if (delayDays) {
+            if (delayDays > 0) {
+                let penaltyAmount = delayDays * 50
+                penaltyAmountEl.value = `${penaltyAmount} DH`
+            }
+        }
+    })
 }
-const closeForm = function(){
+const closeForm = function () {
     overlay.classList.add('hidden');
     formModal.classList.add('hidden');
 }
 addBtn.addEventListener('click', openForm)
-overlay.addEventListener('click',closeForm)
+overlay.addEventListener('click', closeForm)
